bf7bb8525555fbfad25704b92db5543b08fbcbfd
Jhonata Peres <jhonatapers@gmail.com>
2021-10-31 22:04:03 -0300

    tres soluções para oo jantar dos filosofos

--- EPC1/Filosofos.java
+++ EPC1/Filosofos.java
@@ -45,10 +45,13 @@ class Filosofo extends Thread {
 
 class JantaFilosofos {	
 	public static void main(String[] args) {
-		noSolution();
+		//noSolution();
+		//solution1();
+		//solution2();
+		solution3();
 	}	  
 
-	private void noSolution(){
+	private static void noSolution(){
 	    int FIL = 5;
 		 
         Semaphore garfo[] = new Semaphore[FIL]; 
@@ -61,16 +64,56 @@ class JantaFilosofos {
 	}
 
 	//Retira um filosofo
-	private void solution1(){
+	private static void solution1(){
 		int FIL = 5;
 		 
         Semaphore garfo[] = new Semaphore[FIL]; 
 		for (int i=0; i< FIL; i++) {
 		   garfo[i]= new Semaphore(1); 
 	    }
-        for (int i = 0; i < FIL--; i++) {
+
+
+        for (int i = 0; i < FIL-1; i++) {
 			 	 (new Filosofo(i,garfo[i],garfo[(i+1)%(FIL)])).start();	
 		}	
 	}
+
+	//Último filoso troca de mão
+	private static void solution2(){
+		int FIL = 5;
+		 
+        Semaphore garfo[] = new Semaphore[FIL]; 
+		for (int i=0; i< FIL; i++) {
+		   garfo[i]= new Semaphore(1); 
+	    }
+
+        for (int i = 0; i < FIL-1; i++) {
+			 	 (new Filosofo(i,garfo[i],garfo[(i+1)%(FIL)])).start();	
+		}
+
+		(new Filosofo(FIL-1,garfo[FIL - FIL],garfo[(FIL-1)])).start();	
+
+	}
+
+	//Adiciona um garfo Extra para o ultimo filosofo
+	private static void solution3(){
+		int FIL = 5;
+		 
+        Semaphore garfo[] = new Semaphore[FIL+1]; 
+		for (int i=0; i< FIL+1; i++) {
+		   garfo[i]= new Semaphore(1); 
+	    }
+
+		for(int i = 0; i < FIL; i++){
+			
+			if(i == FIL -1){
+				Semaphore garfoExtra = garfo[i+1];
+				(new Filosofo(i,garfo[i],garfoExtra)).start();	
+			}else{
+				(new Filosofo(i,garfo[i],garfo[(i+1)%(FIL)])).start();	
+			}
+		}
+	}
+
 }
 	
